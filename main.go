40786ca04209a7add67a0f54b6e702c76a934e74
Yann Lacroix <yann.lacroix@advans-group.com>
2025-03-15 21:32:34 +0100

    wip

---
+++ main.go
@@ -0,0 +1,168 @@
+package main
+
+import (
+	"crypto/rand"
+	"encoding/base64"
+	"io"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/brawdunoir/kubebrowser/pkg/signals"
+	"github.com/gin-gonic/gin"
+	"k8s.io/client-go/rest"
+	"k8s.io/klog/v2"
+
+	clientset "github.com/brawdunoir/kubebrowser/pkg/client/clientset/versioned"
+	kubeinformers "github.com/brawdunoir/kubebrowser/pkg/client/informers/externalversions"
+)
+
+var (
+	clientID     = os.Getenv("OAUTH2_CLIENT_ID")
+	clientSecret = os.Getenv("OAUTH2_CLIENT_SECRET")
+)
+
+func randString(nByte int) (string, error) {
+	b := make([]byte, nByte)
+	if _, err := io.ReadFull(rand.Reader, b); err != nil {
+		return "", err
+	}
+	return base64.RawURLEncoding.EncodeToString(b), nil
+}
+
+func setCallbackCookie(c *gin.Context, name, value string) {
+	cookie := &http.Cookie{
+		Name:     name,
+		Value:    value,
+		MaxAge:   int(time.Hour.Seconds()),
+		Secure:   c.Request.TLS != nil,
+		HttpOnly: true,
+	}
+	http.SetCookie(c.Writer, cookie)
+}
+
+func main() {
+	klog.InitFlags(nil)
+
+	// set up signals so we handle the shutdown signal gracefully
+	ctx := signals.SetupSignalHandler()
+	logger := klog.FromContext(ctx)
+
+	// creates the in-cluster config
+	cfg, err := rest.InClusterConfig()
+	if err != nil {
+		logger.Error(err, "Error building kubeconfig")
+		klog.FlushAndExit(klog.ExitFlushTimeout, 1)
+	}
+
+	exampleClient, err := clientset.NewForConfig(cfg)
+	if err != nil {
+		logger.Error(err, "Error building kubernetes clientset")
+		klog.FlushAndExit(klog.ExitFlushTimeout, 1)
+	}
+
+	kubeInformerFactory := kubeinformers.NewSharedInformerFactory(exampleClient, time.Second*30)
+
+	lister := kubeInformerFactory.Kubeconfig().V1().Kubeconfigs().Lister()
+
+	kubeconfigs, err := lister.List(nil)
+	if err != nil {
+		logger.Error(err, "Error listing kubeconfigs")
+		klog.FlushAndExit(klog.ExitFlushTimeout, 1)
+	}
+	for _, kubeconfig := range kubeconfigs {
+		logger.Info("Kubeconfig: ", kubeconfig.Name)
+	}
+}
+
+// ctx := context.Background()
+
+// provider, err := oidc.NewProvider(ctx, "https://login.microsoftonline.com/a3594a5e-d561-4f1b-a566-9a93202ecf1d/v2.0")
+// if err != nil {
+// 	log.Fatal(err)
+// }
+// oidcConfig := &oidc.Config{
+// 	ClientID: clientID,
+// }
+// verifier := provider.Verifier(oidcConfig)
+
+// config := oauth2.Config{
+// 	ClientID:     clientID,
+// 	ClientSecret: clientSecret,
+// 	Endpoint:     provider.Endpoint(),
+// 	RedirectURL:  "http://localhost:8080/auth/callback",
+// 	Scopes:       []string{oidc.ScopeOpenID, "profile", "email", oidc.ScopeOfflineAccess},
+// }
+
+// r := gin.Default()
+
+// r.GET("/", func(c *gin.Context) {
+// 	state, err := randString(16)
+// 	if err != nil {
+// 		c.String(http.StatusInternalServerError, "Internal error")
+// 		return
+// 	}
+// 	nonce, err := randString(16)
+// 	if err != nil {
+// 		c.String(http.StatusInternalServerError, "Internal error")
+// 		return
+// 	}
+// 	setCallbackCookie(c, "state", state)
+// 	setCallbackCookie(c, "nonce", nonce)
+
+// 	c.Redirect(http.StatusFound, config.AuthCodeURL(state, oidc.Nonce(nonce)))
+// })
+
+// r.GET("/auth/callback", func(c *gin.Context) {
+// 	state, err := c.Cookie("state")
+// 	if err != nil {
+// 		c.String(http.StatusBadRequest, "state not found")
+// 		return
+// 	}
+// 	if c.Query("state") != state {
+// 		c.String(http.StatusBadRequest, "state did not match")
+// 		return
+// 	}
+
+// 	oauth2Token, err := config.Exchange(ctx, c.Query("code"))
+// 	if err != nil {
+// 		c.String(http.StatusInternalServerError, "Failed to exchange token: "+err.Error())
+// 		return
+// 	}
+// 	rawIDToken, ok := oauth2Token.Extra("id_token").(string)
+// 	if !ok {
+// 		c.String(http.StatusInternalServerError, "No id_token field in oauth2 token.")
+// 		return
+// 	}
+// 	idToken, err := verifier.Verify(ctx, rawIDToken)
+// 	if err != nil {
+// 		c.String(http.StatusInternalServerError, "Failed to verify ID Token: "+err.Error())
+// 		return
+// 	}
+
+// 	nonce, err := c.Cookie("nonce")
+// 	if err != nil {
+// 		c.String(http.StatusBadRequest, "nonce not found")
+// 		return
+// 	}
+// 	if idToken.Nonce != nonce {
+// 		c.String(http.StatusBadRequest, "nonce did not match")
+// 		return
+// 	}
+
+// 	oauth2Token.AccessToken = "*REDACTED*"
+
+// 	resp := struct {
+// 		RefreshToken string
+// 		IDToken      string
+// 	}{oauth2Token.RefreshToken, rawIDToken}
+
+// 	data, err := json.Marshal(resp)
+// 	if err != nil {
+// 		c.String(http.StatusInternalServerError, err.Error())
+// 		return
+// 	}
+// 	c.Data(http.StatusOK, "application/json", data)
+// })
+
+// log.Fatal(r.Run())
---
+++ pkg/apis/kubeconfig/v1/types.go
@@ -0,0 +1,69 @@
+package v1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// KubeconfigList contains a list of Kubeconfig objects
+type KubeconfigList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []Kubeconfig `json:"items"`
+}
+
+// +genclient
+// +genclient:noStatus
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+// +kubebuilder:resource:path=kubeconfigs
+// Kubeconfig is the Schema for the kubeconfigs API
+type Kubeconfig struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+	Spec              KubeconfigSpec `json:"spec,omitempty"`
+}
+
+// KubeconfigSpec defines the desired state of Kubeconfig
+type KubeconfigSpec struct {
+	Name       string `json:"name"`
+	Kubeconfig struct {
+		APIVersion     string    `json:"apiVersion"`
+		Kind           string    `json:"kind"`
+		Clusters       []Cluster `json:"clusters"`
+		Contexts       []Context `json:"contexts,omitempty"`
+		CurrentContext string    `json:"current-context,omitempty"`
+	} `json:"kubeconfig"`
+	Whitelist *Whitelist `json:"whitelist,omitempty"`
+}
+
+// Cluster represents a Kubernetes cluster entry
+type Cluster struct {
+	Name    string  `json:"name"`
+	Cluster Details `json:"cluster"`
+}
+
+// Details holds cluster connection details
+type Details struct {
+	Server                   string `json:"server"`
+	CertificateAuthorityData string `json:"certificate-authority-data,omitempty"`
+	InsecureSkipTLSVerify    bool   `json:"insecure-skip-tls-verify,omitempty"`
+}
+
+// Context represents a context entry
+type Context struct {
+	Name    string      `json:"name"`
+	Context ContextSpec `json:"context"`
+}
+
+// ContextSpec defines the details of a context
+type ContextSpec struct {
+	Cluster string `json:"cluster"`
+	User    string `json:"user"`
+}
+
+// Whitelist contains allowed users/groups
+type Whitelist struct {
+	Users  []string `json:"users,omitempty"`
+	Groups []string `json:"groups,omitempty"`
+}
