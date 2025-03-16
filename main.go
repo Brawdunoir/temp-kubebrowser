f8bd6d4fba092cc811d6a95912c6c41041dda57d
Yann Lacroix <yann.lacroix@advans-group.com>
2025-03-16 14:41:24 +0100

    wip

--- main.go
+++ main.go
@@ -1,45 +1,42 @@
 package main
 
 import (
-	"crypto/rand"
-	"encoding/base64"
-	"io"
-	"net/http"
-	"os"
 	"time"
 
 	"github.com/brawdunoir/kubebrowser/pkg/signals"
-	"github.com/gin-gonic/gin"
+	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/cache"
 	"k8s.io/klog/v2"
 
 	clientset "github.com/brawdunoir/kubebrowser/pkg/client/clientset/versioned"
-	kubeinformers "github.com/brawdunoir/kubebrowser/pkg/client/informers/externalversions"
+
+	informers "github.com/brawdunoir/kubebrowser/pkg/client/informers/externalversions"
 )
 
-var (
-	clientID     = os.Getenv("OAUTH2_CLIENT_ID")
-	clientSecret = os.Getenv("OAUTH2_CLIENT_SECRET")
-)
+// var (
+// 	clientID     = os.Getenv("OAUTH2_CLIENT_ID")
+// 	clientSecret = os.Getenv("OAUTH2_CLIENT_SECRET")
+// )
 
-func randString(nByte int) (string, error) {
-	b := make([]byte, nByte)
-	if _, err := io.ReadFull(rand.Reader, b); err != nil {
-		return "", err
-	}
-	return base64.RawURLEncoding.EncodeToString(b), nil
-}
+// func randString(nByte int) (string, error) {
+// 	b := make([]byte, nByte)
+// 	if _, err := io.ReadFull(rand.Reader, b); err != nil {
+// 		return "", err
+// 	}
+// 	return base64.RawURLEncoding.EncodeToString(b), nil
+// }
 
-func setCallbackCookie(c *gin.Context, name, value string) {
-	cookie := &http.Cookie{
-		Name:     name,
-		Value:    value,
-		MaxAge:   int(time.Hour.Seconds()),
-		Secure:   c.Request.TLS != nil,
-		HttpOnly: true,
-	}
-	http.SetCookie(c.Writer, cookie)
-}
+// func setCallbackCookie(c *gin.Context, name, value string) {
+// 	cookie := &http.Cookie{
+// 		Name:     name,
+// 		Value:    value,
+// 		MaxAge:   int(time.Hour.Seconds()),
+// 		Secure:   c.Request.TLS != nil,
+// 		HttpOnly: true,
+// 	}
+// 	http.SetCookie(c.Writer, cookie)
+// }
 
 func main() {
 	klog.InitFlags(nil)
@@ -61,17 +58,44 @@ func main() {
 		klog.FlushAndExit(klog.ExitFlushTimeout, 1)
 	}
 
-	kubeInformerFactory := kubeinformers.NewSharedInformerFactory(exampleClient, time.Second*30)
+	// Create the informer factory
+	kubeInformerFactory := informers.NewSharedInformerFactory(exampleClient, time.Second*30)
 
-	lister := kubeInformerFactory.Kubeconfig().V1().Kubeconfigs().Lister()
+	// Get the lister for Kubeconfigs
+	kubeconfigLister := kubeInformerFactory.Kubeconfig().V1().Kubeconfigs().Lister()
 
-	kubeconfigs, err := lister.List(nil)
-	if err != nil {
-		logger.Error(err, "Error listing kubeconfigs")
+	// Start the informer factory
+	stopCh := make(chan struct{})
+	kubeInformerFactory.Start(stopCh)
+
+	// Wait for the caches to sync
+	if !cache.WaitForCacheSync(stopCh, kubeInformerFactory.Kubeconfig().V1().Kubeconfigs().Informer().HasSynced) {
+		logger.Error(nil, "Failed to sync caches")
 		klog.FlushAndExit(klog.ExitFlushTimeout, 1)
 	}
-	for _, kubeconfig := range kubeconfigs {
-		logger.Info("Kubeconfig: ", kubeconfig.Name)
+
+	// Use the lister to get the Kubeconfig resource
+	kubeconfig, err := kubeconfigLister.Kubeconfigs("default").Get("example-kubeconfig")
+	if err != nil {
+		logger.Error(err, "Failed to get example-kubeconfig")
+		klog.FlushAndExit(klog.ExitFlushTimeout, 1)
+	}
+
+	logger.Info("I fetched example-kubeconfig once:", "example-kubeconfig", kubeconfig)
+	// time.Sleep(time.Hour * 1)
+
+	for {
+		kubeconfigs, err := kubeconfigLister.Kubeconfigs("default").List(labels.NewSelector())
+		if err != nil {
+			logger.Error(err, "Error listing kubeconfigs")
+			klog.FlushAndExit(klog.ExitFlushTimeout, 1)
+		}
+		for n, kubeconfig := range kubeconfigs {
+
+			logger.Info("this is kubeconfig:", "number", n, "kubeconfig", kubeconfig)
+		}
+		logger.Info("I have listed all kubeconfigs")
+		time.Sleep(time.Second * 10)
 	}
 }
 
