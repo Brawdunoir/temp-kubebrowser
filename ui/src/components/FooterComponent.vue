f84891f55c3c3d27c14ba19c683fbe8383a04655
Yann Lacroix <yann.lacroix@advans-group.com>
2025-04-01 17:28:26 +0200

    release 0.4.0

--- ui/src/components/FooterComponent.vue
+++ ui/src/components/FooterComponent.vue
@@ -4,6 +4,6 @@
 <template>
   <div class="w-full mb-4 flex flex-col items-center text-gray-300">
     <p>Made with ❤️ by Avisto Telecom</p>
-    <p>Version: 0.3.0</p>
+    <p>Version: 0.4.0</p>
   </div>
 </template>
