8a366651784f4096004efed5bd696aafc6d55c79
Yann Lacroix <yann.lacroix@advans-group.com>
2025-04-03 11:00:42 +0200

    internalize fonts

--- ui/src/components/HeaderComponent.vue
+++ ui/src/components/HeaderComponent.vue
@@ -6,7 +6,7 @@ const helpURL = window._env_.HELP_PAGE
 
 <template>
   <header class="flex content-center justify-between p-8 border-b border-gray-600">
-    <h1 class="font-logo font-bold text-xl">KubeBrowser</h1>
+    <h1 class="font-logo font-extrabold text-xl">KubeBrowser</h1>
     <a v-if="helpURL" :href="helpURL" target="_blank" rel="noopener noreferrer">
       <BxSolidHelpCircle class="w-8 h-8"/>
     </a>
