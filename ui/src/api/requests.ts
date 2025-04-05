e7e23c54008238c6eb077271f7cf90f078ae4303
Romain Vincent <romain.vincent@avisto.com>
2025-04-05 15:33:58 +0200

    ♻️  extract the api layer out of vue components
    Even in smaller projects, having clearly defined responsibility layers
    is a must. Different problems requiring different mindset must be
    solved in different regions of the code to avoid mental overflow.

---
+++ ui/src/api/requests.ts
@@ -0,0 +1,38 @@
+import axios from 'axios'
+
+import type { Kubeconfig } from '@/types/Kubeconfig'
+
+export async function getMe(): Promise<string> {
+  if (import.meta.env.DEV) {
+    // Mock response for development
+    return 'Firstname Lastname'
+  } else {
+    // TODO: handle errors?
+    return axios
+      .get<string>('/api/me')
+      .then((res) => res.data)
+      .catch(() => '')
+  }
+}
+
+export async function getConfigs(): Promise<Kubeconfig[]> {
+  if (import.meta.env.DEV) {
+    // Mock response for development
+    return [
+      { name: 'Cluster number 1', kubeconfig: { apiVersion: 'v1', kind: 'Config' } },
+      { name: 'Cluster number 2', kubeconfig: { apiVersion: 'v1', kind: 'Config2' } },
+      { name: 'Cluster number 3', kubeconfig: { apiVersion: 'v1', kind: 'Config2' } },
+      { name: 'Cluster number 4', kubeconfig: { apiVersion: 'v1', kind: 'Config2' } },
+      { name: 'Cluster number 5', kubeconfig: { apiVersion: 'v1', kind: 'Config2' } },
+      { name: 'Cluster number 6', kubeconfig: { apiVersion: 'v1', kind: 'Config2' } },
+      { name: 'Cluster number 7', kubeconfig: { apiVersion: 'v1', kind: 'Config2' } },
+      { name: 'Another cluster', kubeconfig: { apiVersion: 'v1', kind: 'Another' } },
+    ]
+  } else {
+    // TODO: handle errors?
+    return axios
+      .get<Kubeconfig[]>('/api/kubeconfigs')
+      .then((res) => res.data)
+      .catch(() => [])
+  }
+}
--- ui/src/components/AppHello.vue
+++ ui/src/components/AppHello.vue
@@ -1,21 +1,15 @@
 <script setup lang="ts">
-import axios from 'axios'
 import { ref, onMounted } from 'vue'
+
+import * as api from '@/api/requests'
 
 const username = ref('')
 
 const loading = ref(true)
 
 onMounted(async () => {
-  if (import.meta.env.DEV) {
-    // Mock response for development
-    username.value = 'Firstname Lastname'
-    loading.value = false
-  } else {
-    const response = await axios.get<string>('/api/me')
-    username.value = response.data
-    loading.value = false
-  }
+  username.value = await api.getMe()
+  loading.value = false
 })
 </script>
 
