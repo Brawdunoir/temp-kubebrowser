df21d17bba2f46d2f7a555607765d9ac3063b172
Yann Lacroix <yann.lacroix@advans-group.com>
2025-04-08 18:06:40 +0200

    bump to 0.5.1

--- ui/src/components/AppFooter.vue
+++ ui/src/components/AppFooter.vue
@@ -3,6 +3,6 @@
 <template>
   <div class="flex flex-col items-center w-full mb-4 text-gray-300">
     <p>Made with ❤️ by Avisto Telecom</p>
-    <p>Version: 0.5.0</p>
+    <p>Version: 0.5.1</p>
   </div>
 </template>
