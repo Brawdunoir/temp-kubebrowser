21a880c52d85d5d8414d2eeb659dce253521a864
Yann Lacroix <yann.lacroix@advans-group.com>
2025-04-19 11:35:04 +0200

    🚚 leverage pnpm workspaces

--- scripts/buildFrontForBack.sh
+++ scripts/buildFrontForBack.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+SCRIPT_ROOT=$(dirname "${BASH_SOURCE[0]}")/..
+
 rm -r server/kodata
-(cd ui && pnpm run build --base=/home/)
+pnpm build:ui --base=/home/
 cp -r ui/dist server/kodata
